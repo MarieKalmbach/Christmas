91e3ec73d45d5d62f015df8d5c6774b0a7152b81
Marie Kalmbach <Marie.Kalmbach@gmail.com>
2021-12-15 11:04:43 -0600

    Initial Commit

---
+++ Christmas/ChristmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasApp.swift
+//  Christmas
+//
+//  Created by BPSTIL-Pascal-KALMBACH  on 12/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Christmas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Christmas
+//
+//  Created by BPSTIL-Pascal-KALMBACH  on 12/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Merry Christmas!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
